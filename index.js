04278c3d1eb2b6efcd0f46305fd2dd5e81651fa9
Matt Helton <mjoelhelton@me.com>
2018-11-14 07:58:34 -0500

    Adds function to check for four, five, six rolls

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const ceelo = {
+    fourFiveSixCheck: function (hand) {
+        let check = [4, 5, 6,]
+        if( check === hand) {
+            console.log('AUTOMATIC WIN!!')
+        } else {
+            console.log('Roll again!')
+        }
+    },
     roll: function () {
         hand = []
         
@@ -7,36 +15,9 @@ const ceelo = {
             hand.push(dice)
         }
         return console.log(hand.sort())
-        
+        console.log(this.fourFiveSixCheck(hand))
     } 
 }
-
-
-
-const fourFiveSixCheck = (hand) => {
-    let check = [4, 5, 6,]
-    if( check === hand) {
-        console.log('FOUR FIVE SIX! AUTOMATIC')
-    }
-}
-
-roll = () => {
-    hand = []
-    
-    for (let diceRoll = 0; diceRoll < 3; diceRoll++){
-        let dice = Math.floor((Math.random() * 6) + 1)
-        hand.push(dice)
-    }
-    fourFiveSixCheck(hand)
-    hand.toString()
-    hand.sort()
-    return console.log(hand.join(' '))
-    
-}
-
-
-
-
 
 
 ceelo.roll()

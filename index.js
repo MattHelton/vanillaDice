750581a697540d921399124e8192a9c04b7c929f
Matt Helton <mjoelhelton@me.com>
2018-11-15 12:51:52 -0500

    Fixed triple rolls logging to roll again

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function playGame() {
     console.log(d1, d2, d3)
     if(d1 === d2 && d2 === d3) {
         console.log(`Your score is triple ${d1}!`)
-    }
-    if(d1 === d2 && d2 !== d3) {
+    } else if(d1 === d2 && d2 !== d3) {
         console.log(`Your score is ${d3}`)
     } else if (d1 === d3 && d2 !== d1) {
         console.log(`Your score is ${d2}`)

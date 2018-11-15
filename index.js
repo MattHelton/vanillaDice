ab14beffd24b4cc0dadcf800716c72b6aacef22c
Matt Helton <mjoelhelton@me.com>
2018-11-15 08:19:35 -0500

    Adds logic to check for score for all possible rolls except automati win/lose

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ function playGame() {
     if(d1 === d2 && d2 === d3) {
         console.log(`Your score is triple ${d1}!`)
     }
+    if(d1 === d2 && d2 !== d3) {
+        console.log(`Your score is ${d3}`)
+    } else if (d1 === d3 && d2 !== d1) {
+        console.log(`Your score is ${d2}`)
+    } else if (d2 === d3 && d1 !== d2) {
+        console.log(`Your score is ${d1}`)
+    } else {
+        console.log('Roll again!')
+    }
 }
 
 

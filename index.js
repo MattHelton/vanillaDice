4e2dbce11585e0f60417bc641487e07c05b69371
Matt Helton <mjoelhelton@me.com>
2018-11-15 08:10:07 -0500

    Removes faulty win check and simplifies roll function, nesting it in a playGame function.

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-const ceelo = {
-    fourFiveSixCheck: function (hand) {
-        let fourFiveSix = [4, 5, 6,]
-        let oneTwoThreww = [1, 2, 3]
-        if( fourFiveSix === hand) {
-            console.log('AUTOMATIC WIN!!')
-        } else if(oneTwoThree === hand) {
-            console.log('You lose!')
-        } else {
-            console.log('Roll again!')
-        }
-    },
-    roll: function () {
-        hand = []
-        
-        for (let diceRoll = 0; diceRoll < 3; diceRoll++){
-            let dice = Math.floor((Math.random() * 6) + 1)
-            hand.push(dice)
-        }
-        return console.log(hand.sort())
-        console.log(this.fourFiveSixCheck(hand))
+function playGame() {
+   function roll() {
+        return Math.floor((Math.random() * 6)+ 1)
     } 
+
+    let d1 = roll()
+    let d2 = roll()
+    let d3 = roll()
+
+    console.log(d1, d2, d3)
 }
 
 
-ceelo.roll()
+playGame()

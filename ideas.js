15a1a46a2d746d37d1f4f0c2cc26f61295e8bedb
Matt Helton <MJoelHelton@me.com>
2018-11-28 02:26:29 -0500

    Adds js file of different ideas to be refactored and implemented later

---
+++ ideas.js
@@ -0,0 +1,197 @@
+function playCeelo() {
+    function roll() {
+      return Math.floor(Math.random() * 6 + 1);
+    }
+  
+    function results(...dice) {
+      let div = document.createElement("div");
+      let result = document.createTextNode(dice);
+      // div.addClassList('dice')
+      div.appendChild(result);
+  
+      let currentDiv = document.getElementById("results");
+      document.body.insertBefore(div, currentDiv);
+    }
+    let p1 = {
+      name: "Player One",
+      score: 0,
+      getScore: getScore
+    };
+    let p2 = {
+      name: "Player Two",
+      score: 0,
+      getScore: getScore
+    };
+  
+    function getScore() {
+      let hand = [];
+      let d1 = roll();
+      let d2 = roll();
+      let d3 = roll();
+      hand.push(d1, d2, d3);
+      function results(...dice) {
+        let div = document.createElement("div");
+        let result = document.createTextNode(dice);
+        // div.addClassList('dice')
+        div.className = "dice";
+        div.appendChild(result);
+  
+        let currentDiv = document.getElementById("results");
+        document.body.insertBefore(div, currentDiv);
+      }
+      let result = hand.join(" ");
+      results(result);
+      // for (let i = 0; i < hand.length; i++) {
+      //   results(hand[i])
+      // }
+      console.log(`${d1} ${d2} ${d3}`);
+      if (d1 === d2 && d2 === d3) {
+        console.log(`${this.name} score is ${d1}${d1}${d1}!`);
+        return d1 * 111;
+      } else if (d1 === d2 && d2 !== d3) {
+        console.log(`${this.name} score is ${d3}`);
+        return d3;
+      } else if (d1 === d3 && d2 !== d1) {
+        console.log(`${this.name} score is ${d2}`);
+        return d2;
+      } else if (d2 === d3 && d1 !== d2) {
+        console.log(`${this.name} score is ${d1}`);
+        return d1;
+      } else if (d1 + d2 + d3 === 6 && d1 * d2 !== 4) {
+        console.log("One Two Three, you lose!");
+        return 0;
+      } else if (d1 + d2 + d3 === 15 && d1 * d2 !== 25) {
+        console.log("FOUR FIVE SIX! YOU WIN!");
+        return 1000;
+      } else {
+        console.log("Roll again!");
+        this.getScore();
+      }
+    }
+    function playRound() {
+      let playerOneScore = p1.getScore();
+      let playerTwoScore = p2.getScore();
+  
+      if (playerOneScore < playerTwoScore) {
+        console.log(`Player Two wins!`);
+      } else if (playerOneScore > playerTwoScore) {
+        console.log(`Player One wins!`);
+      } else if (playerOneScore === playerTwoScore) {
+        console.log(`It's a tie!`);
+        playRound();
+      } else {
+        console.log(`This shit broke as fuck.`);
+      }
+    }
+    playRound();
+  }
+  // function playCeelo () {
+  //   document.getElementById('playCeelo').addEventListener('click', ceelo())
+  // }
+  function toggleCeelo() {
+    document.getElementById("ceelo").classList.toggle("is-visible");
+  }
+  
+  function playThrees() {
+    // add logic for returning hold array if currentRoll is greater than 5
+    let hold = [];
+    let hand = [];
+    function roll() {
+      return Math.floor(Math.random() * 6 + 1);
+    }
+  
+    function results(...dice) {
+      let div = document.createElement("div");
+      let result = document.createTextNode(dice);
+      // let held = document.createElement("div");
+      // let heldResult = document.createTextNode([...hold]);
+      // div.addClassList('dice')
+      div.className = "potentialDice";
+  
+      div.appendChild(result);
+  
+      // create an options div for potential dice to be selected
+      // create a selected div with actual selections
+      // pop and push from these divs
+  
+      div.addEventListener(
+        "click",
+        function listener(e) {
+          const d = parseInt(e.target.innerHTML, 10);
+          hold.push(d);
+  
+          div.removeEventListener("click", listener, true);
+  
+          hand.splice(hand.indexOf(hand.find(num => num === d)), 1);
+          // console.log(hand.find(num => num === d));
+          console.log("hand: " + hand);
+          console.log("hold: " + hold);
+          document.getElementById("threes").disabled = false;
+        },
+        true
+      );
+  
+      let currentDiv = document.getElementById("results");
+      document.body.insertBefore(div, currentDiv);
+      // document.body.insertBefore(held, currentDiv);
+      // results(hand);
+    }
+  
+    // function holdResults(...dice) {
+    //   let div = document.createElement("div");
+    //   let result = document.createTextNode(dice);
+    //   // div.addClassList('dice')
+    //   div.className = "hold";
+    //   div.appendChild(result);
+  
+    //   let currentDiv = document.getElementById("dice");
+    //   document.body.insertBefore(div, currentDiv);
+    // }
+    function playRound() {
+      // determine number of rolls based available based on size of the div containing selected hand
+  
+      // let d1 = roll();
+      // let d2 = roll();
+      // let d3 = roll();
+      // let d4 = roll();
+      // let d5 = roll();
+      // hand.push(d1, d2, d3, d4, d5);
+      let times = 5 - hold.length;
+      for (let i = 0; i < times; i++) {
+        hand.push(roll());
+      }
+  
+      for (let i = 0; i < hand.length; i++) {
+        results(hand[i]);
+      }
+  
+      // user has hand
+      // button is disabled
+      // user is prompted to click on dice they want to save
+      // at least one item has to be selected before we continue
+      // existing div is cleared when button is enabled
+      // once hold array has reached a size of 5, display the results and remove the roll for threes button
+  
+      document.getElementById("threes").disabled = true;
+  
+      alert("please select one or more numbers to hang on to");
+  
+      function show(elem) {
+        elem.classList.add("is-visible");
+      }
+  
+      function hide(elem) {
+        elem.classList.remove("is-visible");
+      }
+  
+      function toggle(elem) {
+        elem.classList.toggle("is-visible");
+      }
+    }
+    playRound();
+  }
+  
+  // function playThrees () {
+  //   document.getElementById('playThrees').addEventListener('click', threes())
+  // }
+  

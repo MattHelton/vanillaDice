686b1b05b60f3a46329f80da21e4fdbb1f2b7a13
Matt Helton <MJoelHelton@me.com>
2018-12-10 19:36:34 -0500

    Changes event listeners on buttons to call toggleHiddenClass on the buttons I'd like to hide.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { playCeelo } from './Modules/Ceelo.js'
 import { playThrees } from '/Modules/Threes.js'
-import { removeElement, toggleHiddenClass } from './Modules/Game.js'
+import { toggleHiddenClass } from './Modules/Game.js'
 
 let ceeloButton = document.getElementById('ceeloButton')
 let rollCeelo = document.getElementById('ceelo')
@@ -9,13 +9,15 @@ let rollThrees = document.getElementById('threes')
 let reset = document.getElementById('reset')
 
 ceeloButton.addEventListener('click', function () {
-  rollCeelo.classList.toggle('hidden')
-  threesButton.classList.toggle('hidden')
+  toggleHiddenClass(ceeloButton)
+  toggleHiddenClass(threesButton)
+  toggleHiddenClass(rollCeelo)
 })
 
 threesButton.addEventListener('click', function () {
-  rollThrees.classList.toggle('hidden')
-  ceeloButton.classList.toggle('hidden')
+  toggleHiddenClass(threesButton)
+  toggleHiddenClass(ceeloButton)
+  toggleHiddenClass(rollThrees)
 })
 
 rollCeelo.addEventListener('click', function () {

1647896046d0bdbffcb55005c6128f0ed772230b
Matt Helton <MJoelHelton@me.com>
2018-12-10 19:26:04 -0500

    Changes script src in index.html from index.js to App.js.

--- App.js
+++ App.js
@@ -4,16 +4,15 @@ import { removeElement, toggleHiddenClass } from './Modules/Game.js'
 
 let ceeloButton = document.getElementById('ceeloButton')
 let rollCeelo = document.getElementById('ceelo')
+let threesButton = document.getElementById('threesButton')
+let rollThrees = document.getElementById('threes')
+let reset = document.getElementById('reset')
 
 ceeloButton.addEventListener('click', function () {
-  console.log(rollCeelo)
   rollCeelo.classList.toggle('hidden')
   threesButton.classList.toggle('hidden')
 })
 
-let threesButton = document.getElementById('threesButton')
-let rollThrees = document.getElementById('threes')
-let reset = document.getElementById('reset')
 threesButton.addEventListener('click', function () {
   rollThrees.classList.toggle('hidden')
   ceeloButton.classList.toggle('hidden')
